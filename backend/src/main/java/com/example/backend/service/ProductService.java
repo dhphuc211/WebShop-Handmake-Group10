4dd62b8996de2a66b938583f921839d9de9e2dc0
Pluf_nluer <23130240@st.hcmuaf.edu.vn>
2026-01-20 22:30:13 +0700

    update: cap nhat xu li san pham va mot so chuc nang khac

--- backend/src/main/java/com/example/backend/service/ProductService.java
+++ backend/src/main/java/com/example/backend/service/ProductService.java
@@ -44,8 +44,7 @@ public class ProductService {
         return (int) Math.round(percent);
     }
 
-    public List<Product> getAllProducts(int page, int pageSize) {
-        int offset = (page -1 )*pageSize;
+    public List<Product> getAllProducts(int offset, int pageSize) {
         return productDAO.getAllProducts(offset, pageSize);
     }
 
